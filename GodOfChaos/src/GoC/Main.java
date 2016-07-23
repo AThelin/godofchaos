95b4cf1574ef2a0a9a5951db4cfa75b75e81941a
Aandreas Thelin <andreas.thelin@me.com>
2016-07-23 14:33:35 +0200

    Initial checkin of God Of Chaos

---
+++ GodOfChaos/src/GoC/Main.java
@@ -0,0 +1,23 @@
+package GoC;
+
+/**
+ * @class Game
+ * @author Andreas
+ * @date 2016-07-23
+ * 
+ * This is the Main class for Arilila Legends: God Of Chaos.  
+ *
+ */
+public class Main{
+	
+
+	public Main()
+	{
+	}
+
+	public static void main(String[] args)
+	{
+		new Main();
+	}
+
+}
